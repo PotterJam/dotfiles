1a62e7a038c435925dc84a477c47fdb9901403a0
James Potter <jamesrpotter1@gmail.com>
2024-08-30 21:59:52 +0100

    Fix small layout issues

--- keyboard_layouts/corne/config.h
+++ keyboard_layouts/corne/config.h
@@ -20,9 +20,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
-// High tapping term as we're using permissive hold for
-// things where it matters
-#define TAPPING_TERM 250
+#define TAPPING_TERM 200
 
 #define PERMISSIVE_HOLD_PER_KEY
 
