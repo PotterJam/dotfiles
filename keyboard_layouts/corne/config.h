98c9ed5fe2246ce6ab9e306ac0bca74d18d1bee5
James Potter <jamesrpotter1@gmail.com>
2025-05-18 19:59:12 +0100

    new layout

--- keyboard_layouts/corne/config.h
+++ keyboard_layouts/corne/config.h
@@ -22,7 +22,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define TAPPING_TERM 200
 
-#define PERMISSIVE_HOLD_PER_KEY
 #define HOLD_ON_OTHER_KEY_PRESS_PER_KEY
 
 #ifdef RGBLIGHT_ENABLE
