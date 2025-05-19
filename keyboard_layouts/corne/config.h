af315cf32159fbeb68afeb2b2dea31e69c023971
James Potter <jamesrpotter1@gmail.com>
2025-05-19 20:43:37 +0100

    hold on other key press globally

--- keyboard_layouts/corne/config.h
+++ keyboard_layouts/corne/config.h
@@ -22,7 +22,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define TAPPING_TERM 200
 
-#define HOLD_ON_OTHER_KEY_PRESS_PER_KEY
+#define HOLD_ON_OTHER_KEY_PRESS
 
 #ifdef RGBLIGHT_ENABLE
     #define RGBLIGHT_EFFECT_BREATHING
