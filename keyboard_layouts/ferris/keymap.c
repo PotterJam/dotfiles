1523e49d2dd7aea2d2887315364ce2184b55285d
James Potter <jamesrpotter1@gmail.com>
2025-05-26 20:47:55 +0100

    Move most used numbers to the middle

--- keyboard_layouts/ferris/keymap.c
+++ keyboard_layouts/ferris/keymap.c
@@ -32,9 +32,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [NUM] = LAYOUT_split_3x5_2(
         KC_F9, KC_F10, KC_F11, KC_F12, KC_F6, KC_NO, KC_7, KC_8, KC_9, KC_BSLS,
-        OSM(MOD_LSFT), OSM(MOD_LGUI), OSM(MOD_LALT), OSM(MOD_LCTL), KC_F7, KC_NO, KC_4, KC_5, KC_6, KC_0,
-        KC_F2, KC_F3, KC_F4, KC_F5, KC_F8, KC_NO, KC_1, KC_2, KC_3, KC_DOT,
-            KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS)
+        OSM(MOD_LSFT), OSM(MOD_LGUI), OSM(MOD_LALT), OSM(MOD_LCTL), KC_F7, KC_DOT, KC_0, KC_1, KC_2, KC_3,
+        KC_F2, KC_F3, KC_F4, KC_F5, KC_F8, KC_NO, KC_4, KC_5, KC_6, KC_NO,
+            KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
 };
 
 // Shift . is ? and " is _
