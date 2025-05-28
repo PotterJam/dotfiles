b94d59965fc68e2f27b52093892c9525db8148d0
James Potter <jamesrpotter1@gmail.com>
2025-05-28 18:21:08 +0100

    forgot at symbol

--- keyboard_layouts/ferris/config.h
+++ keyboard_layouts/ferris/config.h
@@ -23,6 +23,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define PERMISSIVE_HOLD_PER_KEY
 #define CHORDAL_HOLD
 #define FLOW_TAP_TERM 135
+#define COMBO_TERM 50
 
 #ifdef RGBLIGHT_ENABLE
     #define RGBLIGHT_EFFECT_BREATHING
