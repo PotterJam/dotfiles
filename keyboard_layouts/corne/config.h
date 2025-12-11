8a3035c9adaec688099aae8fd3573a20a125ac1b
James Potter <james@gearset.com>
2025-12-11 17:17:01 +0000

    Use new layout on the corne

--- keyboard_layouts/corne/config.h
+++ keyboard_layouts/corne/config.h
@@ -1,26 +1,11 @@
-/*
-This is the c configuration file for the keymap
-
-Copyright 2012 Jun Wako <wakojun@gmail.com>
-Copyright 2015 Jack Humbert
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 2 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
-
 #pragma once
 
-#define HOLD_ON_OTHER_KEY_PRESS_PER_KEY
+#define PERMISSIVE_HOLD_PER_KEY
+#define CHORDAL_HOLD
+#define FLOW_TAP_TERM 135
+#define COMBO_TERM 50
+#define TAPPING_TERM 200
+#define QUICK_TAP_TERM 150
 
 #ifdef RGBLIGHT_ENABLE
     #define RGBLIGHT_EFFECT_BREATHING
--- keyboard_layouts/corne/keymap.c
+++ keyboard_layouts/corne/keymap.c
@@ -1,62 +1,153 @@
 #include QMK_KEYBOARD_H
 
-#define LA_NAV MO(NAV)
-#define LA_SYM MO(SYM)
-#define LA_NUM MO(NUM)
-
 enum layers {
-    DEF,
-    NAV,
+    GAL,
     SYM,
     NUM,
+    FUN,
+    NAV,
 };
+
+#define LA_SYM OSL(SYM)
+#define LA_NUM OSL(NUM)
+#define LA_FUN OSL(FUN)
+#define LA_NAV LT(NAV, KC_ENT)
+
+#define HRM_N LSFT_T(KC_N)
+#define HRM_R LGUI_T(KC_R)
+#define HRM_T LALT_T(KC_T)
+#define HRM_S LCTL_T(KC_S)
+#define HRM_H LCTL_T(KC_H)
+#define HRM_A LALT_T(KC_A)
+#define HRM_E LGUI_T(KC_E)
+#define HRM_I LSFT_T(KC_I)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [DEF] = LAYOUT_split_3x6_3(
-        KC_NO, KC_B, KC_L, KC_D, KC_C, KC_V, KC_J, KC_Y, KC_O, KC_U, KC_COMM, KC_NO,
-        KC_NO, KC_N, KC_R, KC_T, KC_S, KC_G, KC_P, KC_H, KC_A, KC_E, KC_I, KC_NO,
-        KC_NO, KC_X, KC_Q, KC_M, KC_W, KC_Z, KC_K, KC_F, KC_QUOT, KC_SCLN, KC_DOT, KC_NO,
-                KC_NO, LA_NAV, OSM(MOD_LSFT), KC_SPC, LA_SYM, KC_NO),
-
-    [NAV] = LAYOUT_split_3x6_3(
-        KC_NO, KC_ESC, OSM(MOD_LCTL), KC_TAB, KC_C, KC_V, KC_NO, KC_HOME, KC_UP, KC_END, KC_DEL, KC_NO,
-        KC_NO, OSM(MOD_LSFT), OSM(MOD_LGUI), OSM(MOD_LALT), OSM(MOD_LCTL), KC_ENT, KC_NO, KC_LEFT, KC_DOWN, KC_RGHT, KC_DEL, KC_NO,
-        KC_NO, KC_NO, KC_LGUI, KC_LALT, KC_BSPC, LGUI(KC_Z), KC_NO, KC_BSPC, CW_TOGG, KC_PGUP, KC_PGDN, KC_NO,
-        KC_NO, KC_TRNS, KC_TRNS, KC_ENT, KC_TRNS, KC_NO),
+    [GAL] = LAYOUT_split_3x6_3(
+        KC_HOME, KC_B,    KC_L,    KC_D,    KC_C,    KC_V,       KC_J,    KC_Y,    KC_O,    KC_U,    KC_COMM, KC_END,
+        KC_ESC,  HRM_N,   HRM_R,   HRM_T,   HRM_S,   KC_G,       KC_P,    HRM_H,   HRM_A,   HRM_E,   HRM_I,   KC_SCLN,
+        LA_FUN,  KC_X,    KC_Q,    KC_M,    KC_W,    KC_Z,       KC_K,    KC_F,    KC_QUOT, KC_UNDS, KC_DOT,  KC_NO,
+                          LA_NAV,  KC_SPC,  LA_NUM,              OSM(MOD_LSFT), KC_BSPC, LA_SYM
+    ),
 
     [SYM] = LAYOUT_split_3x6_3(
-        KC_NO, KC_ESC, KC_LBRC, KC_LCBR, KC_LPRN, KC_PIPE, KC_CIRC, KC_RPRN, KC_RCBR, KC_RBRC, KC_DLR, KC_NO,
-        KC_NO, KC_EXLM, KC_PMNS, KC_DQUO, KC_PEQL, KC_AMPR, KC_HASH, OSM(MOD_LCTL), OSM(MOD_LALT), OSM(MOD_LGUI), OSM(MOD_LSFT), KC_NO,
-        KC_NO, KC_PERC, KC_PAST, KC_PPLS, KC_PSLS, KC_GRV, KC_NO, KC_LT, KC_GT, KC_TILD, KC_AT, KC_NO,
-        KC_NO, KC_TRNS, KC_COLN, KC_TRNS, KC_TRNS, KC_NO),
+        KC_PERC, KC_ASTR, KC_DQUO, KC_LCBR, KC_LPRN, KC_LBRC,    KC_RBRC, KC_RPRN, KC_RCBR, KC_AT,   KC_TRNS, KC_NO,
+        KC_HASH, KC_QUES, KC_MINS, KC_EQL,  KC_COLN, KC_AMPR,    KC_NO,   KC_LCTL, KC_LALT, KC_LGUI, KC_LSFT, KC_TRNS,
+        KC_TILD, KC_LT,   KC_BSLS, KC_PLUS, KC_GT,   KC_SLSH,    KC_NO,   KC_GRV,  KC_TRNS, KC_TRNS, KC_TRNS, KC_NO,
+                          KC_TRNS, KC_TRNS, KC_EXLM,             KC_NO,   KC_NO,   KC_NO
+    ),
 
     [NUM] = LAYOUT_split_3x6_3(
-        KC_NO, KC_F9, KC_F10, KC_F11, KC_F12, KC_F6, KC_NO, KC_7, KC_8, KC_9, KC_BSLS, KC_NO,
-        KC_NO, OSM(MOD_LSFT), OSM(MOD_LGUI), OSM(MOD_LALT), OSM(MOD_LCTL), KC_F7, KC_NO, KC_4, KC_5, KC_6, KC_0, KC_NO,
-        KC_NO, KC_F2, KC_F3, KC_F4, KC_F5, KC_F8, KC_NO, KC_1, KC_2, KC_3, KC_DOT, KC_NO,
-        KC_NO, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO)
+        KC_NO,   KC_NO,   KC_NO,   KC_LCBR, KC_LPRN, KC_LBRC,    KC_RBRC, KC_7,    KC_8,    KC_9,    KC_MINS, KC_BSLS,
+        KC_TRNS, KC_LSFT, KC_LGUI, KC_LALT, KC_LCTL, KC_NO,      KC_SLSH, KC_4,    KC_5,    KC_6,    KC_PLUS, KC_TRNS,
+        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_SLSH,    KC_PERC, KC_1,    KC_2,    KC_3,    KC_ASTR, KC_NO,
+                          KC_NO,   KC_NO,   KC_NO,               KC_EQL,  KC_DOT,  KC_0
+    ),
+
+    [FUN] = LAYOUT_split_3x6_3(
+        QK_BOOT, KC_NO,   KC_NO,   KC_VOLD, KC_VOLU, KC_NO,      KC_NO,   KC_F7,   KC_F8,   KC_F9,   KC_F12,  QK_BOOT,
+        KC_NO,   KC_LSFT, KC_LGUI, KC_LALT, KC_LCTL, KC_NO,      KC_NO,   KC_F4,   KC_F5,   KC_F6,   KC_F11,  KC_NO,
+        KC_TRNS, KC_NO,   KC_NO,   G(KC_V), G(KC_C), KC_NO,      KC_NO,   KC_F1,   KC_F2,   KC_F3,   KC_F10,  KC_NO,
+                          KC_NO,   KC_NO,   KC_NO,               KC_NO,   KC_TRNS, KC_TRNS
+    ),
+
+    [NAV] = LAYOUT_split_3x6_3(
+        KC_TRNS, HYPR(KC_1), HYPR(KC_2), HYPR(KC_3), HYPR(KC_4), HYPR(KC_5),    KC_PGUP, KC_CIRC, KC_UP,   KC_DLR,  KC_PGDN, KC_TRNS,
+        KC_TRNS, KC_LSFT,    KC_LGUI,    KC_LALT,    KC_LCTL,    KC_NO,         KC_NO,   KC_LEFT, KC_DOWN, KC_RGHT, KC_DEL,  KC_TRNS,
+        KC_NO,   HYPR(KC_6), HYPR(KC_7), HYPR(KC_8), HYPR(KC_9), HYPR(KC_0),    KC_NO,   KC_TAB,  KC_HOME, KC_END,  KC_NO,   KC_NO,
+                             KC_NO,      KC_NO,      KC_NO,                     KC_TRNS, KC_TRNS, KC_TRNS
+    ),
 };
 
-// Shift . is ? and " is _
-const key_override_t dot_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_DOT, KC_QUES);
-const key_override_t quot_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_QUOT, KC_UNDS);
+const char chordal_hold_layout[MATRIX_ROWS][MATRIX_COLS] PROGMEM = LAYOUT_split_3x6_3(
+    'L', 'L', 'L', 'L', 'L', 'L',    'R', 'R', 'R', 'R', 'R', 'R',
+    'L', 'L', 'L', 'L', 'L', 'L',    'R', 'R', 'R', 'R', 'R', 'R',
+    'L', 'L', 'L', 'L', 'L', 'L',    'R', 'R', 'R', 'R', 'R', 'R',
+              '*', '*', '*',             '*', '*', '*'
+);
 
-const key_override_t* key_overrides[] = {
-    &dot_key_override,
-    &quot_key_override,
-};
-
-bool get_hold_on_other_key_press(uint16_t keycode, keyrecord_t *record) {
+bool get_permissive_hold(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
+        case HRM_N:
+        case HRM_R:
+        case HRM_T:
+        case HRM_S:
+        case HRM_H:
+        case HRM_A:
+        case HRM_E:
+        case HRM_I:
         case LA_NAV:
-        case LA_SYM:
-        case LA_NUM:
             return true;
         default:
             return false;
     }
 }
 
-layer_state_t layer_state_set_user(layer_state_t state) {
-    return update_tri_layer_state(state, NAV, SYM, NUM);
+bool is_flow_tap_key(uint16_t keycode) {
+    if ((get_mods() & (MOD_MASK_CG | MOD_BIT_LALT)) != 0) {
+        return false;
+    }
+    switch (get_tap_keycode(keycode)) {
+        case KC_SPC:
+        case KC_A ... KC_Z:
+        case KC_DOT:
+        case KC_COMM:
+        case KC_UNDS:
+        case KC_QUOT:
+            return true;
+    }
+    return false;
+}
+
+uint16_t get_flow_tap_term(uint16_t keycode, keyrecord_t* record,
+                           uint16_t prev_keycode) {
+    if (is_flow_tap_key(keycode) && is_flow_tap_key(prev_keycode)) {
+        return FLOW_TAP_TERM;
+    }
+    return 0;
+}
+
+enum combo_events {
+    XQ_CW,
+    BK_THIN_ARROW,
+    FW_ARROW,
+    FW_THIN_ARROW,
+    GT_EQ,
+    LT_EQ,
+};
+
+const uint16_t PROGMEM xq_combo[] = {KC_X, KC_Q, COMBO_END};
+const uint16_t PROGMEM bw_thin_arrow_combo[] = {KC_QUES, KC_MINS, COMBO_END};
+const uint16_t PROGMEM fw_arrow_combo[] = {KC_EQL, KC_AMPR, COMBO_END};
+const uint16_t PROGMEM fw_thin_arrow_combo[] = {KC_LT, KC_BSLS, COMBO_END};
+const uint16_t PROGMEM gt_eq_combo[] = {KC_BSLS, KC_PLUS, COMBO_END};
+const uint16_t PROGMEM lt_eq_combo[] = {KC_DQUO, KC_LCBR, COMBO_END};
+
+combo_t key_combos[] = {
+    [XQ_CW] = COMBO(xq_combo, CW_TOGG),
+    [BK_THIN_ARROW] = COMBO_ACTION(bw_thin_arrow_combo),
+    [FW_ARROW] = COMBO_ACTION(fw_arrow_combo),
+    [FW_THIN_ARROW] = COMBO_ACTION(fw_thin_arrow_combo),
+    [GT_EQ] = COMBO_ACTION(gt_eq_combo),
+    [LT_EQ] = COMBO_ACTION(lt_eq_combo),
+};
+
+void process_combo_event(uint16_t combo_index, bool pressed) {
+    switch(combo_index) {
+        case BK_THIN_ARROW:
+            if (pressed) { SEND_STRING("<-"); }
+            break;
+        case FW_ARROW:
+            if (pressed) { SEND_STRING("=>"); }
+            break;
+        case FW_THIN_ARROW:
+            if (pressed) { SEND_STRING("->"); }
+            break;
+        case GT_EQ:
+            if (pressed) { SEND_STRING(">="); }
+            break;
+        case LT_EQ:
+            if (pressed) { SEND_STRING("<="); }
+            break;
+    }
 }
